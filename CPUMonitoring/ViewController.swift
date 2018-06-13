740a43e3b8c8b53f5bad4e4f904f9fa6761bc929
John Allen <johnallen2455@gmail.com>
2018-06-13 15:13:41 -0400

    Initial Commit

---
+++ CPUMonitoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPUMonitoring
+//
+//  Created by John Allen on 6/13/18.
+//  Copyright © 2018 jallen.studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
